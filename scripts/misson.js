be97033647b3aacbbac308a63dbb47143b1f9688
DANU011 <qnfrdms2003@gmail.com>
2023-01-18 17:41:48 +0900

    p0541

--- scripts/misson.js
+++ scripts/misson.js
@@ -2,5 +2,28 @@ const showDice=()=>{
      const n=Math.floor(Math.random()*6)+1
      const s1=document.querySelector("#s1")
      s1.innerHTML=`<img src="./imges/${n}.png"/>`
-     console.log("주사위 선택");
+     console.log(`<img src="./imges/${n}.png"/>`);
 }
+
+//요소 보이기 함수
+const show=(dspS1,dspS2,dspS3,dspBt1,dspBt2)=>{
+   
+     const s1 = document.querySelector("#s1");
+     const s2 = document.querySelector("#s2");
+     const s3 = document.querySelector("#s3");
+     const bt1 = document.querySelector("#bt1");
+     const br2 = document.querySelector("#br2");
+
+     s1.style.display="dspS1";
+     s2.style.display="dspS2";
+     s3.style.display="dspS3";
+     bt1.style.display="dspBt1";
+     bt2.style.display="dspBt2";
+ 
+}
+
+//dom이 로드가 된 후 
+document.addEventListener("DOMContentLoaded",()=>{
+     show("none","block","none","block","none");
+
+})
