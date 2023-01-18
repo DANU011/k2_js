35ed388df6c97d0c3f33c86bd12a0ae6adbe10e9
DANU011 <qnfrdms2003@gmail.com>
2023-01-18 17:48:01 +0900

    p0547

--- scripts/misson.js
+++ scripts/misson.js
@@ -12,18 +12,18 @@ const show=(dspS1,dspS2,dspS3,dspBt1,dspBt2)=>{
      const s2 = document.querySelector("#s2");
      const s3 = document.querySelector("#s3");
      const bt1 = document.querySelector("#bt1");
-     const br2 = document.querySelector("#br2");
+     const bt2 = document.querySelector("#bt2");
 
-     s1.style.display="dspS1";
-     s2.style.display="dspS2";
-     s3.style.display="dspS3";
-     bt1.style.display="dspBt1";
-     bt2.style.display="dspBt2";
+     s1.style.display=dspS1;
+     s2.style.display=dspS2;
+     s3.style.display=dspS3;
+     bt1.style.display=dspBt1;
+     bt2.style.display=dspBt2;
  
-}
+};
 
 //dom이 로드가 된 후 
 document.addEventListener("DOMContentLoaded",()=>{
      show("none","block","none","block","none");
 
-})
+});
